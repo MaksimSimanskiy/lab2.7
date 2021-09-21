8fee193e53ab80b382e4a77d80599ec0b725543e
MaksimSimanskiy <78483044+MaksimSimanskiy@users.noreply.github.com>
2021-09-21 12:37:09 +0300

    Add files via upload

---
+++ individual.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+if __name__ == "__main__":
+    u = set("abcdefghijklmnopqrstuvwxyz")
+    a = {"b", "c", "g", "I", "w"}
+    b = {"e", "g", "h", "q", "w"}
+    c = {"c", "d", "k", "l", "y"}
+    d = {"a", "g", "h", "u", "v", "z"}
+    x = (a.intersection(c)).union(b)
+    print(f"x = {x}")
+    # Найдем дополнения множеств
+    an = u.difference(a)
+    y = (an.difference(d)).union(c.difference(b))
+    print(f"y = {y}")
