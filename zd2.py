d575dfe929c5ed51787d625634f8129ed3d7c02e
MaksimSimanskiy <78483044+MaksimSimanskiy@users.noreply.github.com>
2021-09-21 12:36:47 +0300

    Delete zd2.py

--- zd2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-if __name__ == "__main__":
-    m = (input("введите элемент - "))
-    k = (input("введите элемент 2- "))
-    m = set(m)
-    k = set(k)
-    c = k.intersection(m)
-    print("Общие элементы", c)
